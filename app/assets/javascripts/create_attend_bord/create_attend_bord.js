a545ef7338940d7a49c5d8f5561cdba6fbb07825
wtanabe <tibakenmatudosi@gmail.com>
2019-09-01 15:53:28 +0900

    押下した日程の「日程」「コメント」「日程削除」の欄を削除する機能を追加

--- app/assets/javascripts/create_attend_bord/create_attend_bord.js
+++ app/assets/javascripts/create_attend_bord/create_attend_bord.js
@@ -1,60 +1,20 @@
 window.addEventListener('load', function() {
- 
-    // echo 'Hello World';
-
-    
-
+    // カレンダを表示
     $('#mycalendar').monthly();
 
- //    var add_repeat_switch = document.getElementById('add_repeat_switch');
-	// add_item.addEventListener("click",create_item);
-
-	$('.monthly-day-event').append("<div></div>");
-
-	$('.monthly-day-event').on("click", function(event){
+    // 日付ボタンを押下時、カレンダ下の欄に押下した日程、コメント、日程削除ボタンを追加
+	$(document).on("click", '.monthly-day-event', function(event){
 		var month_array = $('.monthly-header-title-date').text().split(' ');
 		var month = month_array[0].replace("月","")
 		var day = $(this).attr("data-number");
-		console.log(day);
 		var attend_input_table = document.getElementById('attend-input-table');
         html_content = 
-  //       '<div class="attend-input">' +
-		// `<input type="text" value="${month[0]}/${day}"></input>` +
-		// '<input type="text" name=""></input>' +
-		// '<input type="text" name=""></input>' +
-		// '</div>';
 		'<tr>' +
-		`<td><input type="text" class="attend-input-schedule" value="${month[0]}/${day}" readonly></input></td>` +
-		'<td><input type="text" class="attend-input-comment" name=""></input></td>' +
-		'<td><input type="text" class="attend-input-schedule-delete" name=""></input></td>' +
+		`<td class="attend-td-schedule"><input type="text" class="attend-input-schedule" value="${month}/${day}" readonly></input></td>` +
+		'<td class="attend-td-comment"><input type="text" class="attend-input-comment" name=""></input></td>' +
+		'<td class="attend-td-schedule-delete"><input type="button" class="attend-input-schedule-delete" name=""></input></td>' +
 		'</tr>';
 		$('#attend-input-table').append(html_content);
-		// created.insertAdjacentHTML('afterbegin',html_content);
+		console.log("aaa");
 	});
-
-    // $(document.body).on("click touchstart", parent + " .monthly-day-event", function (event) {
-    // 	console.log("test");
-    // });
-
-	console.log("test");
-  //   $(document.body).on("click touchstart", parent + " .monthly-day", function (event) {
-  //   	console.log("aaa");
-  //   	if(options.mode === "event" && options.eventList) {
-		// 		// var	theList = $(parent + " .monthly-event-list"),
-		// 		// 	myElement = document.getElementById(uniqueId + "day" + whichDay),
-		// 		// 	topPos = myElement.offsetTop;
-		// 		// theList.show();
-		// 		// theList.css("transform");
-		// 		// theList.css("transform", "scale(1)");
-		// 		// $(parent + ' .monthly-list-item[data-number="' + whichDay + '"]').show();
-		// 		// theList.scrollTop(topPos);
-		// 		// viewToggleButton();
-		// 		// if(!options.linkCalendarToEventUrl) {
-		// 		// 	event.preventDefault();
-		// 		// }
-		// 	// If picker, pick date
-		// 	console.log("bbb");
-		// }
-  //   });
-
 })
