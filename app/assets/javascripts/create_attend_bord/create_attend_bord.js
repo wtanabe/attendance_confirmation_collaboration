1ac06bdd66de6aa06b7e4988225322ca519921e5
wtanabe <tibakenmatudosi@gmail.com>
2019-09-22 12:40:41 +0900

    一時退避用

--- app/assets/javascripts/create_attend_bord/create_attend_bord.js
+++ app/assets/javascripts/create_attend_bord/create_attend_bord.js
@@ -4,18 +4,19 @@ window.addEventListener('load', function() {
 
   // 日付ボタンを押下時、カレンダ下の欄に押下した日程、コメント、日程削除ボタンを追加
   $(document).on("click", '.monthly-day-event', function(event){
+    //行数取得
+    var row = $('#attend-input-table').prop('rows').length;
     var month_array = $('.monthly-header-title-date').text().split(' ');
     var month = month_array[0].replace("月","")
     var day = $(this).attr("data-number");
     var attend_input_table = document.getElementById('attend-input-table');
     html_content = 
     '<tr>' +
-    `<td class="attend-td-schedule data-id="1"><input type="text" class="attend-input-schedule" value="${month}/${day}" readonly></input></td>` +
+    `<td class="attend-td-schedule data-id="1"><input type="text" name="event_date[${row-1}][event_date]" class="attend-input-schedule" value="${month}/${day}" readonly></input></td>` +
     '<td class="attend-td-comment data-id="1""><input type="text" class="attend-input-comment" name=""></input></td>' +
     '<td class="attend-td-schedule-delete data-id="1"><input type="button" class="attend-input-schedule-delete" name=""></input></td>' +
     '</tr>';
     $('#attend-input-table').append(html_content);
-    console.log("aaa");
   });
 
   $(document).on("click", '.attend-input-schedule-delete', function(event){
--- app/controllers/create_attend_bord_controller.rb
+++ app/controllers/create_attend_bord_controller.rb
@@ -6,17 +6,23 @@ class CreateAttendBordController < ApplicationController
   end
 
   def new
-    @event_date = EventDate.new(event_date_params)
-    @event_date.event_id = 1
-    @event_date.delete_flg = 0
-    @event_date.save
+    puts params[:post]
+    event_date = EventDate.new(event_date_params)
+    event_date_params['type'].each do |r|
+      puts r
+      event_date = r['event_date']
+      event_date.event_id = 1
+      event_date.delete_flg = 0
+      event_date.save
+    end
+    
     # @event_date_comment = EventDateComment.new(event_date_comment_params)
     # @event = Event.new(event_params)
-    event = Event.new
-    event.event_name = "テスト"
-    event.event_content = "テスト"
-    event.delete_flg = 0
-    event.save
+    # event = Event.new
+    # event.event_name = "テスト"
+    # event.event_content = "テスト"
+    # event.delete_flg = 0
+    # event.save
   end
 
   def event_date_params
